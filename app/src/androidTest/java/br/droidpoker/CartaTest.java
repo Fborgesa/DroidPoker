2f2df6f9b8cb0b51ec3e6fc9eaeba8d3b8b308c9
Denis Albuquerque <denisalbuquerque@gmail.com>
2014-09-19 16:51:11 -0300

    novos testes

--- app/src/androidTest/java/br/droidpoker/CartaTest.java
+++ app/src/androidTest/java/br/droidpoker/CartaTest.java
@@ -1,23 +1,33 @@
 package br.droidpoker;
 
-
 import junit.framework.TestCase;
 
 import br.droidpoker.dominio.Carta;
 import br.droidpoker.dominio.Naipe;
-import br.droidpoker.dominio.ValorCarta;
+import br.droidpoker.dominio.TipoCarta;
 
 /**
  * Created by zidenis on 9/18/14.
  */
 public class CartaTest extends TestCase{
-    Carta carta1 = new Carta(ValorCarta.AS, Naipe.PAUS);
-    Carta carta2 = new Carta(ValorCarta.DOIS, Naipe.PAUS);
-    Carta carta3 = new Carta(ValorCarta.AS, Naipe.COPAS);
+    Carta carta1 = new Carta(TipoCarta.AS, Naipe.PAUS);
+    Carta carta2 = new Carta(TipoCarta.DOIS, Naipe.PAUS);
+    Carta carta3 = new Carta(TipoCarta.AS, Naipe.COPAS);
 
     public void testComparacaoDeCartas() {
         assertEquals(carta1.compareTo(carta2),1);
         assertEquals(carta2.compareTo(carta3),-1);
         assertEquals(carta3.compareTo(carta1),0);
+    }
+
+    public void testValorCarta() {
+        assertEquals(carta1.getNome(), TipoCarta.AS);
+        assertEquals(carta2.getNome().getValor(), 2);
+        assertEquals(carta3.getNome().getValor(), carta1.getNome().getValor());
+        assertEquals(carta2.getNome().getNext(), TipoCarta.TRES);
+        assertEquals(carta1.getNome().getNext(), TipoCarta.DOIS);
+        assertEquals(carta1.getNome().getNext(), carta2.getNome());
+        assertEquals(carta1.getNome().getNext(), carta3.getNome().getNext());
+        assertEquals(TipoCarta.DEZ.getNext(),TipoCarta.VALETE);
     }
 }
--- app/src/androidTest/java/br/droidpoker/MaoTest.java
+++ app/src/androidTest/java/br/droidpoker/MaoTest.java
@@ -8,19 +8,56 @@ import br.droidpoker.dominio.*;
  * Created by zidenis on 9/19/14.
  */
 public class MaoTest extends TestCase {
-    Carta carta1 = new Carta(ValorCarta.AS, Naipe.PAUS);
-    Carta carta2 = new Carta(ValorCarta.DOIS, Naipe.PAUS);
-    Carta carta3 = new Carta(ValorCarta.AS, Naipe.COPAS);
-    Mao maoJogador1 = new Mao();
+    Carta carta1;
+    Carta carta2;
+    Carta carta3;
+    Carta carta4;
+    Carta carta5;
+    Carta carta6;
+    Carta carta7;
+    Carta carta8;
+    Mao maoJogador1;
+    Mao maoJogador2;
+    Mao maoJogador3;
+    Mao maoJogador4;
+
+    protected void setUp() {
+        carta1 = new Carta(TipoCarta.AS, Naipe.PAUS);
+        carta2 = new Carta(TipoCarta.DOIS, Naipe.PAUS);
+        carta3 = new Carta(TipoCarta.AS, Naipe.COPAS);
+        carta4 = new Carta(TipoCarta.DEZ, Naipe.OUROS);
+        carta5 = new Carta(TipoCarta.REI, Naipe.ESPADAS);
+        carta6 = new Carta(TipoCarta.DAMA, Naipe.OUROS);
+        carta7 = new Carta(TipoCarta.DEZ, Naipe.COPAS);
+        carta8 = new Carta(TipoCarta.DEZ, Naipe.PAUS);
+        maoJogador1 = new Mao();
+        maoJogador2 = new Mao();
+        maoJogador3 = new Mao();
+        maoJogador4 = new Mao();
+        try {
+            maoJogador1.addCarta(carta1);
+            maoJogador1.addCarta(carta2);
+            maoJogador2.addCarta(carta3);
+            maoJogador2.addCarta(carta4);
+            maoJogador3.addCarta(carta5);
+            maoJogador3.addCarta(carta6);
+            maoJogador4.addCarta(carta7);
+            maoJogador4.addCarta(carta8);
+        }
+        catch (Exception e) {
+            fail();
+        }
+    }
 
     public void testAddCartasMaoJogador() {
+        Mao mao = new Mao();
         try {
-            assertEquals(maoJogador1.numCartas(), 0);
-            maoJogador1.addCarta(carta1);
-            assertEquals(maoJogador1.numCartas(), 1);
-            maoJogador1.addCarta(carta2);
-            assertEquals(maoJogador1.numCartas(), 2);
-            maoJogador1.addCarta(carta3);
+            assertEquals(mao.numCartas(), 0);
+            mao.addCarta(carta1);
+            assertEquals(mao.numCartas(), 1);
+            mao.addCarta(carta2);
+            assertEquals(mao.numCartas(), 2);
+            mao.addCarta(carta3);
             fail("Exception : uma mao nao deve ter mais de 2 cartas");
         }
         catch (Exception e) {
@@ -28,16 +65,26 @@ public class MaoTest extends TestCase {
         }
     }
     public void testMaoJogador() {
-        try {
-            maoJogador1.addCarta(carta1);
-            maoJogador1.addCarta(carta2);
-            Carta[] cartas = maoJogador1.getCartas();
-            assertEquals(cartas[0],carta1);
-            assertEquals(cartas[1],carta2);
-            assertNotSame(cartas[0],carta2);
-        }
-        catch (Exception e) {
-            fail();
-        }
+        Carta[] cartas = maoJogador1.getCartas();
+        assertEquals(cartas[0],carta1);
+        assertEquals(cartas[1],carta2);
+        assertNotSame(cartas[0],carta2);
+    }
+
+    public void testScoreMao() {
+        assertEquals(maoJogador1.getScore(),24);
+        assertEquals(maoJogador2.getScore(),24);
+        assertTrue(maoJogador1.getScore() == maoJogador2.getScore());
+        assertEquals(maoJogador3.getScore(),28);
+        assertTrue(maoJogador1.getScore() < maoJogador3.getScore());
+        assertEquals(maoJogador4.getScore(),30);
+        assertEquals(new Mao().getScore(),0);
+    }
+
+    public void testComparacaoMaos() {
+        assertEquals(maoJogador1.compareTo(new Mao()),1);
+        assertEquals(maoJogador1.compareTo(maoJogador2), 0);
+        assertEquals(maoJogador1.compareTo(maoJogador3), -1);
+        assertEquals(maoJogador4.compareTo(maoJogador3), 1);
     }
 }
