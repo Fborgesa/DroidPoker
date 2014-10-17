26f614ff72688c1a189302507b994fbd84c757c9
tbfreitas <tarcisiodf23@gmail.com>
2014-10-17 19:03:21 -0300

    Comentário Token

--- app/src/main/java/br/droidpoker/domain/Token.java
+++ app/src/main/java/br/droidpoker/domain/Token.java
@@ -2,6 +2,8 @@ package br.droidpoker.domain;
 
 /**
  * Token portado por um Jogador para indicar que ele é o jogador da vez
+ * ou o dealer
+ *
  */
 public class Token {
     private Jogador playerWithToken;
@@ -9,6 +11,9 @@ public class Token {
     public Token(){
     }
 
+    /** Setando o jogador em pose do Token
+     *  @param playerWithToken jogador que pegara o Token
+     */
     public void setPlayerWithToken(Jogador playerWithToken) {
         this.playerWithToken = playerWithToken;
     }
